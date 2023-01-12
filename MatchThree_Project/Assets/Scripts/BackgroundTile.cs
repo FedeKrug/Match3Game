c71356dd9daf19a0d4a060fdd4f534a28557be45
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-01-11 21:31:21 -0300

    Mobile Platform SetUp

---
+++ MatchThree_Project/Assets/Scripts/BackgroundTile.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+
+    void Awake()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ MatchThree_Project/Assets/Scripts/Board.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] private int _height;
+    [SerializeField] private int _width;
+    [SerializeField] private GameObject _backgroundTile;
+    [SerializeField] private BackgroundTile[,] _backgroundTiles;
+    void Awake()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
