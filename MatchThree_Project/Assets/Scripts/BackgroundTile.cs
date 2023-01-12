2695f6f2d8560eef662f0bc191531440f91c4708
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-01-11 22:28:16 -0300

    Grid&TilesCode

--- MatchThree_Project/Assets/Scripts/BackgroundTile.cs
+++ MatchThree_Project/Assets/Scripts/BackgroundTile.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class BackgroundTile : MonoBehaviour
 {
-
+    [SerializeField] private Board _boardReference;
+    [SerializeField] private TileType _tileType;
+    [SerializeField] private int _x, _y;
     void Awake()
     {
         
@@ -15,4 +17,25 @@ public class BackgroundTile : MonoBehaviour
     {
         
     }
+
+    public void Constructor(Board boardManager, int x, int y)
+	{
+        _x = x;
+        _y = y;
+        _boardReference = boardManager;
+	}
+
+    public void ChangePos(int X, int Y)
+	{
+        _x = X;
+        _y = Y;
+	}
+}
+
+public enum TileType
+{
+    Red,
+    Orange,
+    Green,
+    Cyan
 }
--- MatchThree_Project/Assets/Scripts/Board.cs
+++ MatchThree_Project/Assets/Scripts/Board.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    [SerializeField] private int _height;
-    [SerializeField] private int _width;
-    [SerializeField] private GameObject _backgroundTile;
-    [SerializeField] private BackgroundTile[,] _backgroundTiles;
+    [SerializeField] private int _sizeX;
+    [SerializeField] private int _sizeY;
+    [SerializeField] private BackgroundTile [] _tiles;
+    [SerializeField] private BackgroundTile[,] _grid;
     void Awake()
     {
         
     }
 
-   
-    void Update()
+	private void Start()
+	{
+        _grid = new BackgroundTile[_sizeX, _sizeY];
+        for (int i = 0; i < _sizeX; i++)
+        {
+            for (int j = 0; j < _sizeY; j++)
+            {
+                Vector2 tempPos = new Vector2(i, j);
+
+                BackgroundTile backgroundTile = Instantiate(_tiles[Random.Range(0, _tiles.Length)],
+                    tempPos, Quaternion.identity);
+                _grid[i, j] = backgroundTile;
+
+            }
+        }
+    }
+	void Update()
     {
         
     }
+
+    private void SetUp()
+	{
+       
+        
+	}
 }
