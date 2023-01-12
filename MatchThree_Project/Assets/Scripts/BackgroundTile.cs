4b62c62bdac67776a0b7843cddf8ab59b08abf69
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-01-12 18:45:02 -0300

    TilesMovement

--- MatchThree_Project/Assets/Scripts/BackgroundTile.cs
+++ MatchThree_Project/Assets/Scripts/BackgroundTile.cs
@@ -4,38 +4,55 @@ using UnityEngine;
 
 public class BackgroundTile : MonoBehaviour
 {
-    [SerializeField] private Board _boardReference;
-    [SerializeField] private TileType _tileType;
-    [SerializeField] private int _x, _y;
-    void Awake()
-    {
-        
-    }
-
-   
-    void Update()
-    {
-        
-    }
-
-    public void Constructor(Board boardManager, int x, int y)
+	[SerializeField] private Board _boardReference;
+	[SerializeField] private TileType _tileType;
+	[SerializeField] public int x, y;
+	void Awake()
 	{
-        _x = x;
-        _y = y;
-        _boardReference = boardManager;
+
 	}
 
-    public void ChangePos(int X, int Y)
+
+	void Update()
 	{
-        _x = X;
-        _y = Y;
+
 	}
+
+	public void Constructor(Board boardManager, int X, int Y)
+	{
+		this.x = X;
+		this.y = Y;
+		_boardReference = boardManager;
+	}
+
+	public void ChangePos(int X, int Y)
+	{
+		this.x = X;
+		this.y = Y;
+	}
+
+	private void OnMouseDown()
+	{
+		_boardReference.Drag(this);
+		Debug.Log($"Drag {x}, {y}");
+
+	}
+	private void OnMouseOver()
+	{
+		if (Input.GetMouseButtonUp(0))
+		{
+			_boardReference.Drop(this);
+			Debug.Log($"Drop {x}, {y}");
+
+		}
+	}
+
 }
 
 public enum TileType
 {
-    Red,
-    Orange,
-    Green,
-    Cyan
+	Red,
+	Orange,
+	Green,
+	Cyan
 }
--- MatchThree_Project/Assets/Scripts/Board.cs
+++ MatchThree_Project/Assets/Scripts/Board.cs
@@ -4,39 +4,79 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    [SerializeField] private int _sizeX;
-    [SerializeField] private int _sizeY;
-    [SerializeField] private BackgroundTile [] _tiles;
-    [SerializeField] private BackgroundTile[,] _grid;
-    void Awake()
-    {
-        
-    }
+	[SerializeField] private int _sizeX;
+	[SerializeField] private int _sizeY;
+	[SerializeField] private BackgroundTile[] _tiles;
+	[SerializeField] private BackgroundTile[,] _grid;
+
+	private int _dragX = -1;
+	private int _dragY = -1;
+
 
 	private void Start()
 	{
-        _grid = new BackgroundTile[_sizeX, _sizeY];
-        for (int i = 0; i < _sizeX; i++)
-        {
-            for (int j = 0; j < _sizeY; j++)
-            {
-                Vector2 tempPos = new Vector2(i, j);
+		_grid = new BackgroundTile[_sizeX, _sizeY];
+		for (int i = 0; i < _sizeX; i++)
+		{
+			for (int j = 0; j < _sizeY; j++)
+			{
+				SetUp(i, j);
 
-                BackgroundTile backgroundTile = Instantiate(_tiles[Random.Range(0, _tiles.Length)],
-                    tempPos, Quaternion.identity);
-                _grid[i, j] = backgroundTile;
+			}
+		}
+	}
 
-            }
-        }
-    }
-	void Update()
-    {
-        
-    }
-
-    private void SetUp()
+	public void Drag(BackgroundTile tile)
 	{
-       
-        
+		_dragX = tile.x;
+		_dragY = tile.y;
+	}
+
+	public void Drop(BackgroundTile tile)
+	{
+		if (_dragX ==-1 || _dragY == -1) return;
+		
+		SwapTiles(_dragX, _dragY, tile.x, tile.y);
+		_dragX = -1;
+		_dragY = -1;
+	}
+
+	private void SwapTiles(int x1, int x2, int y1, int y2)
+	{
+		if (x1 == x2 && y1 == y2) return;
+		MoveTiles(x1, x2, y1, y2);
+		//comprobar match
+	}
+
+
+	private void MoveTiles(int x1, int x2, int y1, int y2)
+	{
+		if (_grid[x1, y1]!=null)
+		{
+			_grid[x1, y1].transform.position = new Vector3(x2, y2);
+		}
+		if (_grid[x2, y2] != null)
+		{
+			_grid[x2, y2].transform.position = new Vector3(x1, y1);
+		}
+
+		BackgroundTile temp = _grid[x1, y1];
+		_grid[x1, y1] = _grid[x2, y2];
+		_grid[x2, y2] = temp;
+
+		_grid[x1, y1].ChangePos(x1, y1);
+		_grid[x2, y2].ChangePos(x2, y2);
+
+	}
+
+	private void SetUp(int x, int y)
+	{
+
+		Vector2 tempPos = new Vector2(x, y);
+
+		BackgroundTile backgroundTile = Instantiate(_tiles[Random.Range(0, _tiles.Length)],
+			tempPos, Quaternion.identity);
+		backgroundTile.Constructor(this, x, y);
+		_grid[x, y] = backgroundTile;
 	}
 }
